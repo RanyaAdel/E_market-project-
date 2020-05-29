45c9432f10db1851deb86608a5f4990f57e43195
RanyaAdel <raniaadel922@gmail.com>
2020-05-29 15:34:11 -0700

    Added project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.Entity;
+using Emarketproject1.Models;
+using System.IO;
+
+namespace Emarketproject1.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+        public ActionResult Index()
+        {
+            var records = db.Products.ToList();
+            return View(records);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Add()
+        {
+
+            ViewBag.category_id = new SelectList(db.Categories, "Cid", "name");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Product product, HttpPostedFileBase upload)
+        {
+            if (ModelState.IsValid)
+            {
+                String path = Path.Combine(Server.MapPath("~/Images"), upload.FileName);
+                upload.SaveAs(path);
+                product.image = upload.FileName;
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+
+            ViewBag.category_id = new SelectList(db.Categories, "Cid", "name", product.category_id);
+            return View(product);
+        }
+        public ActionResult Delete(int? id)
+        {
+            
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+
+    }
+}
+
+
+
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Emarketproject1.MvcApplication" Language="C#" %>
